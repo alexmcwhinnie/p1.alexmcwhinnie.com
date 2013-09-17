4c13458d0b7b654d60049e420143639f661d12b2
Alex McWhinnie <alexmcwhinnie@gmail.com>
2013-09-17 19:05:03 -0400

    Added styles and doctype information. Also expanded content

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
+<!DOCTYPE HTML>
 <html>
-<head>
-</head>
-<body>
-<h1>Welcome to P1!</h1>
-<p>This is my name</p>
-<p>I am using Sublime Text 2</p>
-<p>This is a bio about me</p>
-</body>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Project 1</title>
+		<link href="styles.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<h1>Project 1</h1>
+		<h2>Alex McWhinnie</h2>
+		<h3>Bio</h3>
+		<p>Tropical by birth, but a New Englander by choice. After traveling the world, I immigrated from Sydney, Australia to one of the coldest places in America. I take advantage of the short, but amazing summers, by gardening and sailing at MIT, and I pass the time in winter skiing and complaining about the cold.</p>
+		<h3>Code Editor</h3>
+		<p>Sublime Text 2 & Adobe Dreamweaver</p>
+	</body>
 </html>
