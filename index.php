d292ae6863c1dbe739e2bd5a4509bd0ed2190618
Alex McWhinnie <alexmcwhinnie@gmail.com>
2013-09-17 19:44:02 -0400

    Added in-text url in index.php and respective selectors in styles.css

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<h1>Project 1</h1>
 		<h2>Alex McWhinnie</h2>
 		<h3>Bio</h3>
-		<p>Tropical by birth, but a New Englander by choice. After traveling the world, I immigrated from Sydney, Australia to one of the coldest places in America. I take advantage of the short, but amazing summers, by gardening and sailing at MIT, and I pass the time in winter skiing and complaining about the cold.</p>
+		<p>Tropical by birth, but a New Englander by choice. After traveling the world, I immigrated from Sydney, Australia to one of the coldest places in America. I take advantage of the short, but amazing summers, by gardening and <a href="http://sailing.mit.edu">sailing at MIT</a>, and I pass the time in winter skiing and complaining about the cold.</p>
 		<h3>Code Editor</h3>
 		<p>Sublime Text 2 & Adobe Dreamweaver</p>
 	</body>
